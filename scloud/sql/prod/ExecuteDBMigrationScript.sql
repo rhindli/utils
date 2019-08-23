4ba0c0da416a72b425c01d2806bb52aa981223a9
rhindli <robert.hindli@bitsoftware.ro>
2019-08-23 11:58:46 +0300

    New How Tos + other small changes

--- scloud/sql/prod/ExecuteDBMigrationScript.sql
+++ scloud/sql/prod/ExecuteDBMigrationScript.sql
@@ -1,3 +1,3 @@
-spool /Users/hrobert/dev/tmp/RunDBMigration1907_2.txt
-@/Users/hrobert/Downloads/DBMigration1907_2.sql
+spool /Users/hrobert/dev/tmp/DBMigration1907.txt
+@/Users/hrobert/Downloads/DBMigration1907.sql
 spool off
